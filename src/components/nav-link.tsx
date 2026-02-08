daa512d5e081ded3b9f45bc57651530e0e8007c8
Tiago Faria <tiago.freitas.faria@gmail.com>
2026-02-08 02:35:01 -0300

    ajuste no navlink

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -3,11 +3,9 @@ type NavLinkProps = React.ComponentProps<'a'>
 export function NavLink(props: NavLinkProps) {
 	return (
 		<li className="text-white text-lg m-6 group relative w-max cursor-pointer">
-			<span className="absolute -top-1 right-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
-			<a className="" {...props}>
-				{props.children}
-			</a>
-			<span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full"></span>
+			<span className="absolute -top-1 right-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full" />
+			<a {...props}>{props.children}</a>
+			<span className="absolute -bottom-1 left-0 w-0 transition-all duration-300 h-0.5 bg-white group-hover:w-full" />
 		</li>
 	)
 }
