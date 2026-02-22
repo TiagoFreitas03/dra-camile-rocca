cc1a4d1d25e85e1b980af78eb7c71fd18d229423
Tiago Faria <tiago.freitas.faria@gmail.com>
2026-02-22 14:18:25 -0300

    ajuste na imagem da navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,9 @@ import { NavLink } from './nav-link'
 export function Navbar() {
 	return (
 		<nav className="bg-dark-red opacity-90 flex fixed top-0 w-full z-50">
-			<img src={headerLogo} className="w-30 2xl:w-40 h-auto" />
+			<div className="flex items-center">
+				<img src={headerLogo} className="w-30 2xl:w-40 h-auto" />
+			</div>
 
 			<ul className="uppercase flex gap-12 list-none m-auto font-cinzel">
 				<NavLink href="#home">home</NavLink>
