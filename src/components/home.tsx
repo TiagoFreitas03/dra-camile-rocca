33a7d437de399a4c070feaab82a42a8aad43c01e
Tiago Faria <tiago.freitas.faria@gmail.com>
2026-03-07 18:48:10 -0300

    tornando a seção home responsiva

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,30 +1,30 @@
 import { ContactButton } from './utils/contact-button'
 import { DraCamileRocca } from './utils/dra-camile-rocca'
-import { Section } from './utils/section'
 import { WhatsAppLink } from './utils/whatsapp-link'
 
 export function Home() {
 	return (
-		<Section backgroundImageClass="md:bg-[url(/home-bg.png)]" id="home">
-			<div className="flex justify-around w-full">
-				<span className="hidden md:block" />
-
-				<div className="flex flex-col gap-24 justify-center items-center">
-					<div className="text-center">
+		<div
+			className="sm:aspect-2/1 h-screen sm:h-full overflow-hidden bg-[url(/home-bg-mobile.png)] sm:bg-[url(/home-bg.png)] bg-no-repeat bg-cover sm:bg-contain"
+			id="home"
+		>
+			<div className="w-full h-full flex justify-center items-start sm:items-center sm:justify-end">
+				<div className="flex flex-col gap-4 md:gap-12 justify-center items-center sm:w-[60%]">
+					<div className="text-center pt-24 sm:pt-0">
 						<DraCamileRocca />
 
-						<span className="font-inter text-3xl md:text-4xl font-light text-dark-red block mt-4">
+						<span className="font-inter text-2xl lg:text-3xl 2xl:text-4xl font-light text-dark-red block mt-2 sm:mt-4">
 							Cirurgiã-dentista
 						</span>
 					</div>
 
-					<WhatsAppLink className="hidden xl:block">
-						<ContactButton className="font-cinzel text-2xl py-4 px-11">
+					<WhatsAppLink className="hidden sm:block">
+						<ContactButton className="font-cinzel lg:text-xl 2xl:text-2xl py-3 px-6 md:py-4 md:px-10">
 							AGENDE SUA CONSULTA
 						</ContactButton>
 					</WhatsAppLink>
 				</div>
 			</div>
-		</Section>
+		</div>
 	)
 }
--- src/components/utils/dra-camile-rocca.tsx
+++ src/components/utils/dra-camile-rocca.tsx
@@ -1,6 +1,6 @@
 export function DraCamileRocca() {
 	return (
-		<h1 className="font-cinzel-decorative font-extralight text-3xl md:text-5xl 2xl:text-7xl text-dark-red">
+		<h1 className="font-cinzel-decorative font-extralight text-3xl lg:text-5xl 2xl:text-7xl text-dark-red">
 			DRa. Camile Rocca
 		</h1>
 	)
